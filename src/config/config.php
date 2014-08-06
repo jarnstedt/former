bb39fbd8d2b747ef73e96c9b3bc1b39f8aa787f1
Joonas Järnstedt <joonas.jarnstedt@mediasignal.fi>
2014-08-06 13:24:54 +0300

    Add bootstrap config option.

--- src/config/config.php
+++ src/config/config.php
@@ -2,25 +2,36 @@
 // Set default settings here
 return array(
 
+    // Default form class
     'formClass' => 'form-horizontal',
 
-    'autocomplete' => 'off',
-
+    // Use inputs name as id
     'nameAsId' => true,
 
+    // Id prefix
     'idPrefix' => 'field_',
 
+    // Required field label syntax
     'requiredLabel' => '.req',
 
+    // Added before required label
     'requiredPrefix' => '',
 
+    // Added after required label
     'requiredSuffix' => ' *',
 
+    // Class for required fields
     'requiredClass' => 'label-required',
 
+    // Class for fields with error
     'controlGroupError' => 'has-error',
 
+    // Show error texts
     'displayInlineErrors' => true,
 
-    'commentClass' => 'help-block'
+    // Class for comments
+    'commentClass' => 'help-block',
+
+    // Use bootsrap styling
+    'bootstrap' => true
 );
